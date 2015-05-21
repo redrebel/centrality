38bfe0df76c67fe18c670c83d05ed05477367e86
Lee junggeun <cjred77@gmail.com>
2015-05-22 00:37:32 +0900

    c

--- src/closeness/ClosenessCentrality.java
+++ src/closeness/ClosenessCentrality.java
@@ -1,4 +1,4 @@
-package ccloseness;
+package closeness;
 
 import java.util.HashMap;
 import java.util.List;
--- src/main/CentralityMain.java
+++ src/main/CentralityMain.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Map;
 
-import ccloseness.ClosenessCentrality;
+import closeness.ClosenessCentrality;
 import eigenvector.EigenvectorCentrality;
 import betweenness.BetweennessCentrality;
 
